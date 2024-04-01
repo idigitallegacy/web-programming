e31edc64fd7887ae8d3ef71c6e7cbef173ae8757
idigitallegacy <99473127+idigitallegacy@users.noreply.github.com>
2024-04-01 03:45:00 +0300

    lab-5 (x2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from "./users/users.module";
 import { PostsModule } from "./posts/posts.module";
 import { CommentsModule } from "./comments/comments.module";
-import { AuthModule } from './auth/auth.module';
 import * as process from "process";
 
 @Module({
