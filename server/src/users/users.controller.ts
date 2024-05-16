4e23d2226f7a8ac96f5d1d16992c50f0a16bcee7
idigitallegacy <99473127+idigitallegacy@users.noreply.github.com>
2024-05-16 14:18:33 +0300

    lab-6 (x11)

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -89,7 +89,7 @@ export class UsersController {
               break
             }
             case "PROD": {
-              response.cookie("vk_access_token", exchangedToken.response.access_token, { maxAge: exchangedToken.response.expires_in, secure: true, httpOnly: true, domain: "web-y25-makarov.onrender.com", sameSite: "none", path: "/" });
+              response.cookie("vk_access_token", exchangedToken.response.access_token, { maxAge: exchangedToken.response.expires_in, secure: true, httpOnly: true, domain: "localhost", sameSite: "none", path: "/" });
               break
             }
           }
